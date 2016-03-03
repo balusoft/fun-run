256dddf70baa61108ee5f7c3d6418187a5e5762d
Balaji Cherukuri <balajicherukuri@balajis-mbp.wlan.netapp.com>
2016-03-03 16:34:16 -0500

    Completed BtPerm.cc

--- BtPerm.cc
+++ BtPerm.cc
@@ -39,6 +39,42 @@ private:
   uint32_t _curNum;
 };
 
+void backtrack(
+  std::vector<uint32_t> &perm,
+  std::vector<bool> &inUse,
+  uint32_t &level,
+  std::vector<std::vector<uint32_t>> &solutions) {
+  if (perm.size()-1 == level) {
+    solutions.push_back(perm);
+  } else {
+    NextNumsItr itr(inUse);
+    ++level;
+    while (itr.next()) {
+      perm[level] = *itr;
+      inUse[*itr] = true;
+      backtrack(perm, inUse, level, solutions);
+      perm[level] = 0;
+      inUse[*itr] = false;
+    }
+    --level;
+  }
+}
+
+void genPerm(uint32_t num) {
+  std::vector<std::vector<uint32_t>> perms;
+  // discard '0' to avoid nastiness
+  std::vector<uint32_t> perm(num+1, 0);
+  std::vector<bool> inUse(num+1, false);
+  uint32_t level=0;
+  backtrack(perm, inUse, level, perms);
+  for (const std::vector<uint32_t> &perm : perms) {
+    for (uint32_t val : perm) {
+      std::cout << val << " ";
+    }
+    std::cout << "\n";
+  }
+}
+
 void print_next_nums (const std::vector<bool>& inUse) {
   NextNumsItr itr(inUse);
   std::cout << "inUse: ";
@@ -53,6 +89,8 @@ void print_next_nums (const std::vector<bool>& inUse) {
 }
 
 int main() {
+  genPerm(3);
+  return 0;
   {
     const std::vector<bool> inUse = {false/*disc*/, false, true, false, true, false, false, true, false, false, true, false, false};
     print_next_nums(inUse);
@@ -63,3 +101,34 @@ int main() {
     print_next_nums(inUse);
   }
 }
+
+#if 0
+balajis-mbp:CPP Test9 balajicherukuri$ clang++ -g  -std=c++11 BtPerm.cc  && valgrind ./a.out
+==60021== Memcheck, a memory error detector
+==60021== Copyright (C) 2002-2015, and GNU GPL'd, by Julian Seward et al.
+==60021== Using Valgrind-3.11.0 and LibVEX; rerun with -h for copyright info
+==60021== Command: ./a.out
+==60021==
+0 1 2 3
+0 1 3 2
+0 2 1 3
+0 2 3 1
+0 3 1 2
+0 3 2 1
+==60021==
+==60021== HEAP SUMMARY:
+==60021==     in use at exit: 26,203 bytes in 192 blocks
+==60021==   total heap usage: 275 allocs, 83 frees, 32,595 bytes allocated
+==60021==
+==60021== LEAK SUMMARY:
+==60021==    definitely lost: 0 bytes in 0 blocks
+==60021==    indirectly lost: 0 bytes in 0 blocks
+==60021==      possibly lost: 0 bytes in 0 blocks
+==60021==    still reachable: 4,096 bytes in 1 blocks
+==60021==         suppressed: 22,107 bytes in 191 blocks
+==60021== Rerun with --leak-check=full to see details of leaked memory
+==60021==
+==60021== For counts of detected and suppressed errors, rerun with: -v
+==60021== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+balajis-mbp:CPP Test9 balajicherukuri$
+#endif
