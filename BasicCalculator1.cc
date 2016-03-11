19e031bb050f77b705836b1a754a256732244aa8
Balaji Cherukuri <balusoft@gmail.com>
2016-03-10 22:07:58 -0500

    Adding Calc problem definitions

---
+++ BasicCalculator1.cc
@@ -0,0 +1,18 @@
+/*
+224. Basic Calculator My Submissions Question
+Total Accepted: 24326 Total Submissions: 113565 Difficulty: Medium
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string may contain open ( and closing parentheses ), the plus +
+or minus sign -, non-negative integers and empty spaces .
+
+You may assume that the given expression is always valid.
+
+Some examples:
+"1 + 1" = 2
+" 2-1 + 2 " = 3
+"(1+(4+5+2)-3)+(6+8)" = 23
+Note: Do not use the eval built-in library function.
+
+Subscribe to see which companies asked this question
+*/
