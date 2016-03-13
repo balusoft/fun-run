bb689651c2c280f63c059b934a522aee9b48ad2c
Balaji Cherukuri <balusoft@gmail.com>
2016-03-13 17:23:00 -0400

    Initial version of infix2postfix, with iterator pattern, and recursion

--- BasicCalculator1.cc
+++ BasicCalculator1.cc
@@ -78,5 +78,125 @@ Converting from infix to postfix:
       add to result and then push operator to stack.
 *) Now if there are any operators left add them to result.
 
+Evaluating postfix expression:
+-----------------------------
+*) Iterate postfix from left-to-right
+**) if elem is operand push to stack
+**) else if elem is operator:
+***) operand2=pop();
+***) operand1=pop();
+***) push(perform_operation(elem, operand1, operand2));
+*) Now stack should be having only one elem, that is expression result.
 
+Note: we can combine those two steps and evaluate directly with two stacks
+as well
 */
+
+#include <cassert>
+#include <ctype.h>
+#include <string>
+#include <iostream>
+#include <stack>
+#include <map>
+
+/*
+example expression:
+2 * 3 * 31 + 2
+assume delim is sapce
+*/
+class Infix2PostfixItr {
+public:
+  Infix2PostfixItr(const std::string &infixExpr)
+      : infix(infixExpr), operators{{'+',1}, {'-',1}, {'*',2}, {'/',2}},
+        oprStack(), curInfIdx(0), delim(' '), curValue() {}
+  bool next() {
+    if (curInfIdx >= infix.length() && oprStack.empty()) {
+      return false;
+    }
+    if (curInfIdx >= infix.length()) {
+      if (!oprStack.empty()) {
+        curValue = oprStack.top();
+        oprStack.pop();
+        return true;
+      }
+    } else {
+      // If delimiter
+      while (infix[curInfIdx] == ' ') {
+        ++curInfIdx;
+      }
+      if (curInfIdx >= infix.length()) {
+        return next();
+      }
+      // If operand
+      std::string oprnd;
+      while (isdigit(infix[curInfIdx])) {
+        oprnd += infix[curInfIdx];
+        ++curInfIdx;
+      }
+      if (!oprnd.empty()) {
+        curValue = oprnd;
+        return true;
+      }
+      // If operator
+      if (operators.find(infix[curInfIdx]) != operators.end()) {
+        if (oprStack.empty() || oprStack.top() == '(' ||
+            operators[oprStack.top()] <= operators[infix[curInfIdx]]) {
+          oprStack.push(infix[curInfIdx]);
+          ++curInfIdx;
+          return next();
+        } else {
+          curValue = oprStack.top();
+          oprStack.pop();
+          return true;
+        }
+      } else if (infix[curInfIdx] == '(') {
+        oprStack.push(infix[curInfIdx]);
+        ++curInfIdx;
+        return next();
+      } else if (infix[curInfIdx] == ')') {
+        if (oprStack.top() == '(') {
+          oprStack.pop();
+          ++curInfIdx;
+          return next();
+        } else {
+          curValue = oprStack.top();
+          oprStack.pop();
+          return true;
+        }
+      } else {
+        assert(0 == "should never reach here");
+      }
+    }
+    return false;
+  }
+  std::string operator*() const {
+    return curValue;
+  }
+private:
+  const std::string& infix;
+  std::map<char, uint32_t> operators;
+  std::stack<char> oprStack;
+  size_t curInfIdx;
+  char delim;
+  std::string curValue;
+};
+std::string infix2postfix(const std::string &infix) {
+  std::string postfix;
+  Infix2PostfixItr itr(infix);
+  while(itr.next()) {
+    postfix += *itr;
+    postfix += ' ';
+  }
+  return postfix;
+}
+
+int main() {
+  std::cout << "1+2 = " << infix2postfix("1+2") << "\n";
+  std::cout << "1*2+3 = " << infix2postfix("1*2+3") << "\n";
+  std::cout << "1+2*3 = " << infix2postfix("1+2*3") << "\n";
+  std::cout << "    1 + 2      *      3 = " << infix2postfix("        1   + 2    * 3  ") << "\n";
+  std::cout << "(1+2)*3 = " << infix2postfix("(1+2)*3") << "\n";
+  std::cout << "((1+2))*3 = " << infix2postfix("(((((1+2))*3)))") << "\n";
+  return 0;
+}
+
