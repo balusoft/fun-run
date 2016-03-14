a6d93e2515b9330fd355d2f2b35820d0493f3d43
Balaji Cherukuri <balusoft@gmail.com>
2016-03-13 20:21:53 -0400

    Leet code accepted

--- BasicCalculator1.cc
+++ BasicCalculator1.cc
@@ -108,66 +108,84 @@ class Infix2PostfixItr {
 public:
   Infix2PostfixItr(const std::string &infixExpr)
       : infix(infixExpr), operators{{'+',1}, {'-',1}, {'*',2}, {'/',2}},
-        oprStack(), curInfIdx(0), delim(' '), curValue() {}
-  bool next() {
-    if (curInfIdx >= infix.length() && oprStack.empty()) {
+        oprStack(), idx(0), curInfIdx(0), delim(' '), curValue() {}
+  bool next2() {
+    if (idx >= infix.length() && oprStack.empty()) {
       return false;
     }
-    if (curInfIdx >= infix.length()) {
-      if (!oprStack.empty()) {
-        curValue = oprStack.top();
-        oprStack.pop();
-        return true;
+    if (idx == infix.length()) {
+      curValue = oprStack.top();
+      oprStack.pop();
+      return true;
+    }
+    for (; idx < infix.length(); ++idx) {
+      // handle delimiter
+      while (idx < infix.length() && infix[idx] == ' ') {
+        ++idx;
       }
-    } else {
-      // If delimiter
-      while (infix[curInfIdx] == ' ') {
-        ++curInfIdx;
+      if (idx == infix.length()) {
+        return next2();
       }
-      if (curInfIdx >= infix.length()) {
-        return next();
-      }
-      // If operand
+      //infix[i] could be operator or operand.
+      // in case of operand return
       std::string oprnd;
-      while (isdigit(infix[curInfIdx])) {
-        oprnd += infix[curInfIdx];
-        ++curInfIdx;
+      while (idx < infix.length() && isdigit(infix[idx])) {
+        oprnd += infix[idx];
+        ++idx;
       }
       if (!oprnd.empty()) {
         curValue = oprnd;
         return true;
       }
-      // If operator
-      if (operators.find(infix[curInfIdx]) != operators.end()) {
-        if (oprStack.empty() || oprStack.top() == '(' ||
-            operators[oprStack.top()] <= operators[infix[curInfIdx]]) {
-          oprStack.push(infix[curInfIdx]);
-          ++curInfIdx;
-          return next();
-        } else {
-          curValue = oprStack.top();
-          oprStack.pop();
-          return true;
+      // Now check for operator
+      if (operators.find(infix[idx]) != operators.end()) {
+        // There are three cases here:
+        // 1) stack is empty, in that case just push it
+        if (oprStack.empty()) {
+          oprStack.push(infix[idx]);
+          continue;
         }
-      } else if (infix[curInfIdx] == '(') {
-        oprStack.push(infix[curInfIdx]);
-        ++curInfIdx;
-        return next();
-      } else if (infix[curInfIdx] == ')') {
+        // 1.1) another case is top of the stack could be '('
         if (oprStack.top() == '(') {
+          oprStack.push(infix[idx]);
+          continue;
+        }
+        // 2) stack top has less priority operator, in this case just push it
+        if (operators[oprStack.top()] < operators[infix[idx]]) {
+          oprStack.push(infix[idx]);
+          continue;
+        }
+        // 3) stack top has high or equal proirity operator, in this case just
+        //    pop it. and then push the operator to stack
+        if (operators[oprStack.top()] >= operators[infix[idx]]) {
+          curValue = oprStack.top();
           oprStack.pop();
-          ++curInfIdx;
-          return next();
+          oprStack.push(infix[idx]);
+          ++idx;
+          return true;
+        }
+      }
+      // Now check for parenthesis
+      // There are two cases here
+      // case 1: it is '(', just push it
+      if ('(' == infix[idx]) {
+        oprStack.push(infix[idx]);
+        continue;
+      } else if (')' == infix[idx]) {
+        // case 2: it is '(', just push it
+        // Again here two cases, top could be '(' or operator
+        // case '(' just pop it.
+        if ('(' == oprStack.top()) {
+          oprStack.pop();
+          continue;
         } else {
           curValue = oprStack.top();
           oprStack.pop();
           return true;
         }
-      } else {
-        assert(0 == "should never reach here");
       }
     }
-    return false;
+    return next2();
   }
   std::string operator*() const {
     return curValue;
@@ -177,26 +195,75 @@ private:
   std::map<char, uint32_t> operators;
   std::stack<char> oprStack;
   size_t curInfIdx;
+  int idx;
   char delim;
   std::string curValue;
 };
+int performOp(char op, int oprnd1, int oprnd2) {
+  switch (op) {
+  case '+':
+    return oprnd1 + oprnd2;
+  case '-':
+    return oprnd1 - oprnd2;
+  case '*':
+    return oprnd1 * oprnd2;
+  case '/':
+    return oprnd1 / oprnd2;
+  default:
+    assert(0 == "never ");
+  }
+  return 0;
+}
+int exprEval(const std::string &expr) {
+  Infix2PostfixItr itr(expr);
+  std::stack<int> oprndStack;
+  while(itr.next2()) {
+    // Two possible outcomes here, output could be operator or operand.
+    // case 1: operand, push it to stack
+    if (isdigit((*itr)[0])) {
+      oprndStack.push(atoi((*itr).c_str()));
+    } else {
+      // case 2: operator, pop operands and perform operation
+      int oprnd2 = oprndStack.top();
+      oprndStack.pop();
+      int oprnd1 = oprndStack.top();
+      oprndStack.pop();
+      assert ((*itr).length() == 1);
+      int res = performOp((*itr)[0], oprnd1, oprnd2);
+      oprndStack.push(res);
+    }
+  }
+  assert(oprndStack.size() == 1);
+  return oprndStack.top();
+}
 std::string infix2postfix(const std::string &infix) {
   std::string postfix;
   Infix2PostfixItr itr(infix);
-  while(itr.next()) {
+  while(itr.next2()) {
     postfix += *itr;
     postfix += ' ';
   }
   return postfix;
 }
 
+class Solution {
+  public:
+    int calculate(std::string s) {
+      return exprEval(s);
+    }
+};
+
 int main() {
-  std::cout << "1+2 = " << infix2postfix("1+2") << "\n";
-  std::cout << "1*2+3 = " << infix2postfix("1*2+3") << "\n";
-  std::cout << "1+2*3 = " << infix2postfix("1+2*3") << "\n";
-  std::cout << "    1 + 2      *      3 = " << infix2postfix("        1   + 2    * 3  ") << "\n";
-  std::cout << "(1+2)*3 = " << infix2postfix("(1+2)*3") << "\n";
-  std::cout << "((1+2))*3 = " << infix2postfix("(((((1+2))*3)))") << "\n";
+  std::cout << "(1+(4+5+2)-3)+(6+8) : " << infix2postfix("(1+(4+5+2)-3)+(6+8)") << "\n";
+  std::cout << "(1+(4+5+2)-3)+(6+8)" << exprEval("(1+(4+5+2)-3)+(6+8)") << "\n";
+  return 0;
+  std::cout << "1+2 = " << exprEval("1+2") << "\n";
+  std::cout << "1*2+3 = " << exprEval("1*2+3") << "\n";
+  std::cout << "1+2*3 = " << exprEval("1+2*3") << "\n";
+  std::cout << "    1 + 2      *      3 = " << exprEval("        1   + 2    * 3  ") << "\n";
+  std::cout << "(1+2)*3 = " << exprEval("(1+2)*3") << "\n";
+  std::cout << "((1+2))*3 = " << exprEval("(((((1+2))*3)))") << "\n";
+  std::cout << "1+2+3 = " << exprEval("1+2+3") << "\n";
   return 0;
 }
 
