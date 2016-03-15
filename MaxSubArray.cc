b92455bb079e5b4c1880e0917a776c9fbdf039a8
Balaji Cherukuri <balusoft@gmail.com>
2016-03-15 12:02:23 -0400

    adding new problem MaxSubArray.cc

---
+++ MaxSubArray.cc
@@ -0,0 +1,22 @@
+/*
+53. Maximum Subarray My Submissions Question
+Total Accepted: 102841 Total Submissions: 283598 Difficulty: Medium
+Find the contiguous subarray within an array (containing at least one number)
+which has the largest sum.
+
+For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+
+click to show more practice.
+
+Subscribe to see which companies asked this question
+
+Hide Tags Divide and Conquer Array Dynamic Programming
+Show Similar Problems
+
+*/
+
+class Solution {
+public:
+  int maxSubArray(vector<int> &nums) {}
+};
