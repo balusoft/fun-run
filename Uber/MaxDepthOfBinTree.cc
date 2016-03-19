3b225a17fb84ddbf35161e5db207cf908360c002
Balaji Cherukuri <balusoft@gmail.com>
2016-03-19 13:52:18 -0400

    LC accepteted

--- Uber/MaxDepthOfBinTree.cc
+++ Uber/MaxDepthOfBinTree.cc
@@ -12,16 +12,58 @@ Show Tags
 Show Similar Problems
 
 */
-/**
- * Definition for a binary tree node.
- */
+
+#include <iostream>
+#include <algorithm>
+
 struct TreeNode {
   int val;
   TreeNode *left;
   TreeNode *right;
   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
+std::ostream& operator<< (std::ostream& ostr, const TreeNode* t) {
+  if (t) {
+    ostr << "(val: " << t->val << ", l:";
+    if (t->left) {
+      ostr << t->left->val;
+    } else {
+      ostr << "nullptr";
+    }
+    ostr << ", r:";
+    if (t->right) {
+      ostr << t->right->val;
+    } else {
+      ostr << "nullptr";
+    }
+    ostr << ")";
+  } else {
+    ostr << "(nullptr)";
+  }
+  return ostr;
+}
 class Solution {
 public:
-  int maxDepth(TreeNode *root) {}
+  int maxDepth(TreeNode *root) {
+    if (!root) {
+      return 0;
+    }
+    int height =0;
+    height = maxDepth(root->left);
+    height = std::max(height, maxDepth(root->right));
+    return height + 1;
+  }
 };
+
+int main() {
+  TreeNode t(10);
+  TreeNode tl(5);
+  TreeNode tr(15);
+  t.left=&tl;
+  std::cout << "t: " << &t << "\n";
+  TreeNode *pt=nullptr;
+  std::cout << "pt: " << pt << "\n";
+  Solution s;
+  std::cout << "depth: " << s.maxDepth(&t) << "\n";
+  return 0;
+}
