527197aad2e5786965c84b6d4c9f2dac49956150
Balaji Cherukuri <balusoft@gmail.com>
2016-03-19 13:51:49 -0400

    LC accepted

--- Uber/RevLinkedList.cc
+++ Uber/RevLinkedList.cc
@@ -14,17 +14,51 @@ Discuss
 
 */
 
-/**
- * Definition for singly-linked list.
- */
+#include <iostream>
 struct ListNode {
   int val;
   ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {}
+  ListNode(int x) : val(x), next(nullptr) {}
 };
-class Solution {
-  public:
-    ListNode* reverseList(ListNode* head) {
+std::ostream& operator<<(std::ostream& ostr, ListNode *list) {
+  ostr << "[ ";
+  ListNode *itr = list;
+  while (itr) {
+    std::cout << itr->val << ",";
+    itr = itr->next;
+  }
+  ostr << "]";
+  return ostr;
+}
 
+class Solution {
+public:
+  ListNode *reverseList(ListNode *head) {
+    ListNode *rev = nullptr;
+    ListNode *itr = head;
+    while (itr) {
+      ListNode *tmp = itr;
+      itr = itr->next;
+      tmp->next = rev;
+      rev = tmp;
     }
+    return rev;
+  }
 };
+
+void test(ListNode* list) {
+  Solution s;
+  std::cout << "List: " <<  list << "\n";
+  std::cout << "Rev List: " << s.reverseList(list) << "\n\n";
+  return;
+}
+
+int main() {
+  ListNode l(10);
+  ListNode l2(20);
+  l.next = &l2;
+  ListNode l3(30);
+  l2.next = &l3;
+  test (&l);
+  test (nullptr);
+}
