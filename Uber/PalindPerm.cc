9aa6506daad4636d533e5dab05686ba3c2c30db3
Balaji Cherukuri <balusoft@gmail.com>
2016-03-20 15:11:24 -0400

    Try for LC submit

--- Uber/PalindPerm.cc
+++ Uber/PalindPerm.cc
@@ -2,12 +2,13 @@
 /*
 266. Palindrome Permutation My Submissions Question
 Total Accepted: 10101 Total Submissions: 20121 Difficulty: Easy
-Given a string, determine if a permutation of the string could form a palindrome.
+Given a string, determine if a permutation of the string could form a
+palindrome.
 
 For example,
 "code" -> False, "aab" -> True, "carerac" -> True.
 
-Show Hint 
+Show Hint
 Show Company Tags
 Show Tags
 Show Similar Problems
@@ -16,9 +17,43 @@ Discuss
 
 
 */
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+std::ostream &operator<<(std::ostream &ostr,
+                         const std::unordered_set<char> &s) {
+  ostr << "{";
+  for (auto c : s) {
+    ostr << c << ", ";
+  }
+  ostr << "}";
+  return ostr;
+}
 class Solution {
 public:
-    bool canPermutePalindrome(string s) {
-        
+  bool canPermutePalindrome(const std::string &s) {
+    if (s.empty()) {
+      return true;
     }
+    std::unordered_set<char> uniqueChars;
+    for (auto c : s) {
+      uniqueChars.insert(c);
+    }
+    return ((s.length() / 2) == uniqueChars.size()) ||
+           ((1 + s.length() / 2) == uniqueChars.size());
+  }
 };
+
+void test(const std::string& s) {
+  Solution sol;
+  std::cout << "Str: " << s << std::boolalpha
+            << " << >> res: " << sol.canPermutePalindrome(s) << "\n";
+  return;
+}
+
+int main() {
+  test("code");
+  test("aab");
+  test("carerac");
+}
