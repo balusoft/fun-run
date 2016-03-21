2869867223ee9b56963c64891690171f5cd1be27
Balaji Cherukuri <balusoft@gmail.com>
2016-03-21 14:45:21 -0400

    run changes

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 ./cxxtest-4.3/bin/cxxtestgen --error-printer -o ${1}.cc $1
 /usr/bin/clang++ -I ./cxxtest-4.3/ -g -Werror -std=c++11 ${1}.cc
 valgrind ./a.out
-rm ./a.out
+rm -rf ./a.out*
+rm ./${1}.cc
