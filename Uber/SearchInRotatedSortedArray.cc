facd1160446a172b8575c860fe6629b81e1d9ea8
Balaji Cherukuri <balusoft@gmail.com>
2016-03-23 10:41:43 -0400

    LC accepted for search in rotated sorted array

--- Uber/SearchInRotatedSortedArray.cc
+++ Uber/SearchInRotatedSortedArray.cc
@@ -14,10 +14,69 @@ Subscribe to see which companies asked this question
 
 Show Tags
 Show Similar Problems
+4 5 6 0 1 2 3
+2 3 4 5 6 0 1
 
+search for 2:
+
+mid = 0, low=4, high=3
 */
+
+#include <vector>
+#include <cxxtest/TestSuite.h>
 
 class Solution {
 public:
-  int search(vector<int> &nums, int target) {}
+  int search(const std::vector<int> &nums, int target) {
+    int low=0;
+    int high=nums.size()-1;
+    //if (nums[low] < nums[high]) {
+    //  return bin_search(nums, low, high, target);
+    //}
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      }
+      // key is right hand side :-)
+      // case : if rt in order (nums[mid+1] < nums[high])
+      if (mid+1 == high || nums[mid+1] < nums[high]) {
+        // if target in rt range move right
+        if (target >= nums[mid+1] && target <= nums[high]) {
+          low = mid+1;
+        // else move left
+        } else {
+          high = mid-1;
+        }
+      } else {
+        // Now left is inorder
+        // check wither target in range of left
+        // if low == mid break, means no more chance
+        if (low == mid) {
+          break;
+        }
+        if (target >= nums[low] && target <= nums[mid-1]) {
+          high = mid-1;
+        } else {
+          low = mid + 1;
+        }
+      }
+    }
+    return -1;
+  }
+};
+
+class TestSolution : public CxxTest::TestSuite {
+public:
+  void test_example() {
+    Solution s;
+    TS_ASSERT_EQUALS(s.search({5, 6, 7, 0, 1, 2, 3, 4}, 3), 6);
+    TS_ASSERT_EQUALS(s.search({5, 6, 7, 0, 1, 2, 3, 4}, 0), 3);
+    TS_ASSERT_EQUALS(s.search({5, 6, 7, 0, 1, 2, 3, 4}, 0), 3);
+    TS_ASSERT_EQUALS(s.search({0, 1, 2, 3, 4}, 0), 0);
+    TS_ASSERT_EQUALS(s.search({5, 6, 7, 8, 0, 1, 2, 3, 4}, 0), 4);
+    TS_ASSERT_EQUALS(s.search({0, 1, 2, 3, 4}, 2), 2);
+    TS_ASSERT_EQUALS(s.search({0, 1, 2, 3, 4}, 3), 3);
+    TS_ASSERT_EQUALS(s.search({0, 1, 2, 3, 4}, 4), 4);
+  }
 };
