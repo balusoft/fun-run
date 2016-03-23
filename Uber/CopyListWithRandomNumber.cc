076192dde8bdfce3f028ac7c2b1dcfd9fe1799c6
Balaji Cherukuri <balusoft@gmail.com>
2016-03-23 14:31:58 -0400

    LC accepted for copy list with random pointer

--- Uber/CopyListWithRandomNumber.cc
+++ Uber/CopyListWithRandomNumber.cc
@@ -6,25 +6,79 @@ A linked list is given such that each node contains an additional random pointer
 which could point to any node in the list or null.
 
 Return a deep copy of the list.
-
-Subscribe to see which companies asked this question
-
-Show Tags
-Show Similar Problems
-Have you met this question in a real interview? Yes  No
-Discuss
-
-
 */
-/**
-  Definition for singly-linked list with a random pointer.
-  */
+
+#include <iostream>
+#include <unordered_map>
+
+#include <cxxtest/TestSuite.h>
+
 struct RandomListNode {
   int label;
   RandomListNode *next, *random;
   RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
 };
+
 class Solution {
 public:
-  RandomListNode *copyRandomList(RandomListNode *head) {}
+  RandomListNode *copyRandomList(RandomListNode *head) {
+    if (!head) {
+      return nullptr;
+    }
+    std::unordered_map<RandomListNode*, RandomListNode*> map;
+    RandomListNode *itr = head;
+    RandomListNode *copy = nullptr;
+    RandomListNode *cItr = copy;
+
+    while (itr) {
+      RandomListNode *tmp = new RandomListNode(itr->label);
+      map[itr] = tmp;
+      if (cItr) {
+        cItr->next = tmp;
+        cItr = cItr->next;
+      } else {
+        cItr = tmp;
+        copy = cItr;
+      }
+      itr = itr->next;
+    }
+    itr = head;
+    while (itr) {
+      if (itr->random) {
+        map[itr]->random = map[itr->random];
+      }
+      itr = itr->next;
+    }
+    return copy;
+  }
+};
+
+class TestSuite : public CxxTest::TestSuite {
+public:
+  void test_random() {
+    RandomListNode r0(0);
+    RandomListNode r1(1);
+    RandomListNode r2(2);
+
+    r0.next = &r1;
+    r1.next = &r2;
+
+    r2.random = &r1;
+    r1.random = &r0;
+
+    Solution s;
+    RandomListNode *copy = s.copyRandomList(&r0);
+    TS_ASSERT_EQUALS(copy->label, 0);
+    TS_ASSERT_EQUALS(copy->next->label, 1);
+    TS_ASSERT_EQUALS(copy->next->next->label, 2);
+    TS_ASSERT_EQUALS(copy->next->next->next, nullptr);
+
+    TS_ASSERT_EQUALS(copy->random, nullptr);
+    TS_ASSERT_EQUALS(copy->next->random->label, 0);
+    TS_ASSERT_EQUALS(copy->next->next->random->label, 1);
+  }
+  void test_null() {
+    Solution s;
+    TS_ASSERT_EQUALS(s.copyRandomList(nullptr), nullptr);
+  }
 };
