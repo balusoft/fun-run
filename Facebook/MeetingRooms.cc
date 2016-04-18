f33bb766c19fa5d72995193423989143d72e8f68
Balaji Cherukuri <balusoft@gmail.com>
2016-04-18 18:47:40 -0400

    LC accepted

--- Facebook/MeetingRooms.cc
+++ Facebook/MeetingRooms.cc
@@ -1,6 +1,5 @@
 /**
-
-ven an array of meeting time intervals consisting of start and end times
+Given an array of meeting time intervals consisting of start and end times
 [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all
 meetings.
 
@@ -18,7 +17,22 @@ struct Interval {
   Interval() : start(0), end(0) {}
   Interval(int s, int e) : start(s), end(e) {}
 };
+
 class Solution {
 public:
-  bool canAttendMeetings(vector<Interval> &intervals) {}
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        if (intervals.size() < 2) {
+            return true;
+        }
+        std::sort(intervals.begin(), intervals.end(),
+                  [](const Interval &x, const Interval &y) {
+                    return x.start < y.start;
+                  });
+        for (size_t idx=1; idx<intervals.size(); ++idx) {
+            if (intervals[idx].start < intervals[idx-1].end) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
