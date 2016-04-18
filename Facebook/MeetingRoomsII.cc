6aaeceb45ee76d6d2693ac933512e2e5f2b6eb5d
Balaji Cherukuri <balusoft@gmail.com>
2016-04-18 19:33:09 -0400

    LC accepted

--- Facebook/MeetingRoomsII.cc
+++ Facebook/MeetingRoomsII.cc
@@ -22,9 +22,43 @@ Show Similar Problems
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
 class Solution {
 public:
-    int minMeetingRooms(vector<Interval>& intervals) {
-        
+  int minMeetingRooms(vector<Interval> &intervals) {
+    if (intervals.size() < 2) {
+      return intervals.size();
     }
+    std::sort(intervals.begin(), intervals.end(),
+              [](const Interval &i1, const Interval &i2) {
+                return i1.start < i2.start;
+              });
+    auto cmp = [](const Interval &i1, const Interval &i2) {
+      return i1.end > i2.end;
+    };
+    std::priority_queue<Interval, std::vector<Interval>, decltype(cmp)> pq(cmp);
+    pq.push(intervals[0]);
+    for (int i = 1; i < intervals.size(); ++i) {
+      Interval top = pq.top();
+      pq.pop();
+      if (top.end <= intervals[i].start) {
+        top.end = intervals[i].end;
+      } else {
+        pq.push(intervals[i]);
+      }
+      pq.push(top);
+    }
+    return pq.size();
+  }
+  bool overlaps(const Interval &i1, const Interval &i2) {
+    return i1.start >= i2.start && i1.start <= i2.end;
+  }
 };
