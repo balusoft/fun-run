eec889fecc9879c4c979b150332284d33a10d315
Balaji Cherukuri <balusoft@gmail.com>
2016-04-19 17:32:00 -0400

    LC accepted

--- Facebook/GraphValidTree.cc
+++ Facebook/GraphValidTree.cc
@@ -29,7 +29,69 @@ Show Similar Problems
 
 */
 
+using Graph = std::vector<std::list<int>>;
+enum class VertexState {
+  UNDISCOVERED,
+  DISCOVERED,
+  PROCESSED,
+};
 class Solution {
 public:
-  bool validTree(int n, vector<pair<int, int>> &edges) {}
+  bool validTree(int n, vector<pair<int, int>> &edges) {
+    Graph g(n);
+    makeGraph(g, edges);
+    std::vector<int> p(n, -1);
+    std::vector<VertexState> state(n, VertexState::UNDISCOVERED);
+    if (!dfs(g, 0, state, p)) {
+      return false;
+    }
+    for (int i = 1; i < n; ++i) {
+      if (state[i] != VertexState::UNDISCOVERED) {
+        continue;
+      }
+      // no need to proceed with forest of trees...
+      // we got our answer.. this is not tree..
+      return false;
+    }
+  }
+  void makeGraph(Graph &g, const vector<pair<int, int>> &edges) {
+    for (auto &e : edges) {
+      g[e.first].push_back(e.second);
+      g[e.second].push_back(e.first);
+    }
+  }
+  bool dfs(const Graph &g, const int v, std::vector<VertexState> &state,
+           std::vector<int> &p) {
+    state[v] = VertexState::DISCOVERED;
+    for (int u : g[v]) {
+      // std::cout << "(u, v) : " << "(" << u << "," << v << ")\n";
+      // std::cout << "state[u]: " <<
+      // static_cast<std::underlying_type<VertexState>::type>(state[u]) << "\n";
+      // std::cout << "p[u]: " << p[u] << "\n";
+      switch (state[u]) {
+      // if state is undiscovered, this is tree edge.
+      case VertexState::UNDISCOVERED:
+        p[u] = v;
+        if (!dfs(g, u, state, p)) {
+          return false;
+        }
+        break;
+      // if state is DISCOVERED, this is back edge.
+      case VertexState::DISCOVERED:
+        // unless this edge is with your parent it is back edge
+        if (p[v] != u) {
+          return false;
+        }
+        break;
+      // if vertex is already processed, this is back edge too.
+      // but we should have caught this already?
+      case VertexState::PROCESSED:
+        return false;
+        break;
+      }
+    }
+    state[v] = VertexState::PROCESSED;
+    // std::cout << "4vertex end v: " << v << std::endl;
+    return true;
+  }
 };
