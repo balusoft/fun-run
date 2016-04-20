e131449cfbdb0f01b915874a596f3de2f45b474f
Balaji Cherukuri <balusoft@gmail.com>
2016-04-19 21:16:27 -0400

    LC accepted

--- Facebook/NumberOfIslands.cc
+++ Facebook/NumberOfIslands.cc
@@ -20,8 +20,29 @@ Example 2:
 00011
 Answer: 3
 */
-
 class Solution {
 public:
-  int numIslands(vector<vector<char>> &grid) {}
+  int numIslands(vector<vector<char>> &grid) {
+    int ic = 0;
+    for (int i = 0; i < grid.size(); ++i) {
+      for (int j = 0; j < grid[0].size(); ++j) {
+        if (grid[i][j] == '1') {
+          ++ic;
+          markIsland(grid, i, j);
+        }
+      }
+    }
+    return ic;
+  }
+  void markIsland(vector<vector<char>> &grid, const int x, const int y) {
+    if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() ||
+        grid[x][y] != '1') {
+      return;
+    }
+    grid[x][y] = 'X';
+    markIsland(grid, x - 1, y);
+    markIsland(grid, x + 1, y);
+    markIsland(grid, x, y - 1);
+    markIsland(grid, x, y + 1);
+  }
 };
