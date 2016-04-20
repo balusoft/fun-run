75a78a887ee417f756da011887d764f7516af031
Balaji Cherukuri <balusoft@gmail.com>
2016-04-20 19:51:59 -0400

    strstr kmp

--- MyStrStr.cpp
+++ MyStrStr.cpp
@@ -5,6 +5,7 @@
 //  Created by Balaji Cherukuri on 7/29/15.
 //  Copyright (c) 2015 FooBar. All rights reserved.
 //
+// http://jakeboxer.com/blog/2009/12/13/the-knuth-morris-pratt-algorithm-in-my-own-words/
 
 #include <string>
 #include <vector>
